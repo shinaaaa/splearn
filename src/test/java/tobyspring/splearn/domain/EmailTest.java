cc0ef57052dcfea8154d302296ce9eb5d8ae2063
shinaaaa <shin029abc@gmail.com>
2025-07-06 21:10:14 +0900

    feat: 도메인 모델의 값 객체 도입

---
+++ src/test/java/tobyspring/splearn/domain/EmailTest.java
@@ -0,0 +1,16 @@
+package tobyspring.splearn.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EmailTest {
+
+    @Test
+    void equality() {
+        var email1 = new Email("shin@splearn.app");
+        var email2 = new Email("shin@splearn.app");
+
+        assertThat(email1).isEqualTo(email2);
+    }
+}
--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -104,4 +104,13 @@ class MemberTest {
 
         assertThat(member.isActive()).isFalse();
     }
+
+    @Test
+    void invalidEmail() {
+        assertThatThrownBy(() ->
+                Member.create(new MemberCreateRequest("kim", "shin", "secret"), passwordEncoder)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+        Member.create(new MemberCreateRequest("kim@splearn.app", "shin", "secret"), passwordEncoder);
+    }
 }
