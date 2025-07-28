7ed40d1dea8f2b69a611247307349d3db2224739
shinaaaa <shin029abc@gmail.com>
2025-07-29 00:33:02 +0900

    feat: 엔티티 클래스와 JPA 매핑 정보 분리

--- src/main/java/tobyspring/splearn/domain/Member.java
+++ src/main/java/tobyspring/splearn/domain/Member.java
@@ -1,12 +1,11 @@
 package tobyspring.splearn.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.annotations.NaturalId;
-import org.hibernate.annotations.NaturalIdCache;
 
 import static java.util.Objects.requireNonNull;
 import static org.springframework.util.Assert.state;
@@ -15,7 +14,6 @@ import static org.springframework.util.Assert.state;
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@NaturalIdCache
 public class Member extends AbstractEntity {
 
     @NaturalId
