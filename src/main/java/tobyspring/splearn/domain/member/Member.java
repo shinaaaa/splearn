334618cdb32982bb7be4fef5e3bcbcfe6a38be92
shinaaaa <shin029abc@gmail.com>
2025-08-03 12:25:30 +0900

    feat: Member 애그리거트 개발

--- src/main/java/tobyspring/splearn/domain/member/Member.java
+++ src/main/java/tobyspring/splearn/domain/member/Member.java
@@ -1,18 +1,25 @@
-package tobyspring.splearn.domain;
+package tobyspring.splearn.domain.member;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.annotations.NaturalId;
+import tobyspring.splearn.domain.AbstractEntity;
+import tobyspring.splearn.domain.shared.Email;
+
+import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
 import static org.springframework.util.Assert.state;
 
 @Entity
 @Getter
-@ToString(callSuper = true)
+@ToString(callSuper = true, exclude = "detail")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends AbstractEntity {
 
@@ -25,6 +32,7 @@ public class Member extends AbstractEntity {
 
     private MemberStatus status;
 
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private MemberDetail detail;
 
     public static Member register(MemberRegisterRequest createRequest, PasswordEncoder passwordEncoder) {
@@ -35,6 +43,8 @@ public class Member extends AbstractEntity {
         member.passwordHash = requireNonNull(passwordEncoder.encode(createRequest.password()));
         member.status = MemberStatus.PENDING;
 
+        member.detail = MemberDetail.create();
+
         return member;
     }
 
@@ -42,12 +52,14 @@ public class Member extends AbstractEntity {
         state(this.status == MemberStatus.PENDING, "PENDING 상태가 아닙니다.");
 
         this.status = MemberStatus.ACTIVE;
+        this.detail.activate();
     }
 
     public void deactivate() {
         state(this.status == MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다.");
 
         this.status = MemberStatus.DEACTIVATED;
+        this.detail.deactivate();
     }
 
     public boolean verifyPassword(String password, PasswordEncoder passwordEncoder) {
@@ -56,6 +68,12 @@ public class Member extends AbstractEntity {
 
     public void changeNickname(String nickname) {
         this.nickname = requireNonNull(nickname);
+    }
+
+    public void updateInfo(MemberInfoUpdateRequest updateRequest) {
+        this.nickname = Objects.requireNonNull(updateRequest.nickname());
+
+        this.detail.updateInfo(updateRequest);
     }
 
     public void changePassword(String password, PasswordEncoder passwordEncoder) {
---
+++ src/test/java/tobyspring/splearn/domain/member/ProfileTest.java
@@ -0,0 +1,38 @@
+package tobyspring.splearn.domain.member;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ProfileTest {
+
+    @Test
+    void profile() {
+        new Profile("tobyspring");
+        new Profile("toby123");
+        new Profile("12345");
+    }
+
+    @Test
+    void profileFail() {
+        assertThatThrownBy(() -> new Profile(""))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Profile("toolongtoolongtoolong"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Profile("A"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Profile("프로필"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void url() {
+        var profile = new Profile("tobyspring");
+
+        assertThat(profile.url()).isEqualTo("@tobyspring");
+    }
+}
